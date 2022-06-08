e4020b875017304160955ef762af814d132db07f
LekeneCedric <93648451+LekeneCedric@users.noreply.github.com>
2022-06-08 11:38:29 -0400

    removr unecessary files

--- node_modules/ionic-stepper/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import IonicStepperModule from './ionic-stepper.module';
-export { IonicStepperModule } from './ionic-stepper.module';
-export { IonicStepperComponent } from './ionic-stepper';
-export { IonicStepComponent } from './ionic-step';
-export { IonicStepHeaderComponent } from './ionic-step-header';
-export { IonicStepperNext, IonicStepperPrevious } from './ionic-stepper-button';
-export default IonicStepperModule;
